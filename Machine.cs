7136ba9ded778d8238500d52e71a53c052458ace
FiloCarletti <filocarletti123@gmail.com>
2025-04-29 12:10:11 +0200

    initial machine class and I/O from console

---
+++ Machine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineConfigurator
+{
+    class Machine
+    {
+        int id { get; set; } 
+        double power;
+        int axes;
+        Material material;
+
+        public Machine()
+        {
+
+        }
+        public Machine(int id, double power, int axes, Material material)
+        {
+            this.id = id;
+            this.power = power;
+            this.axes = axes;
+            this.material = material;
+        }
+        
+        public void toString()
+        {
+            Console.WriteLine("Machine ID: "+id+" \nPower: "+power+" Watts\nTotal Axes: "+axes+"\nWorking Material: " + material.ToString());
+        } /*
+        public string toString()
+        {
+            return "Machine ID: " + id + " \nPower: " + power + " Watts\nTotal Axes: " + axes + "\nWorking Material: " + material.ToString();
+        }*/
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace MachineConfigurator
     {
         static void Main(string[] args)
         {
+            double power = 0;
+            int axes = 0;
+            string material = "Steel";
+
+            Console.Write("Type the machine power: ");
+            power = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Type the machine axes: ");
+            axes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type the machine working material: ");
+            material = Console.ReadLine();
+
+
+            Machine m = new Machine(0, power, axes, (Material)Enum.Parse(typeof(Material), material));
+
+            m.toString();
+            //Console.WriteLine(m.toString());
         }
     }
 }
